394714d57ee551dc6d92147885fe4fa3455b2840
pushap <pushap.tyagi@crossml.com>
2025-12-31 10:56:39 +0530

    image commit

--- Image_prompting/index.py
+++ Image_prompting/index.py
@@ -10,9 +10,7 @@ api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise ValueError("GEMINI_API_KEY not found. Check your .env file.")
 
-# ---------------------------
-# Initialize Gemini Client
-# ---------------------------
+
 client = genai.Client(api_key=api_key)
 
 def run_experiment(
